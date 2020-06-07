9e6ef793995c4b074e4cc5903b04c6bf2623c9df
praful chandra <praful.chandra@yahoo.com>
2020-06-07 19:04:04 +0530

    changes made

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,5 +1,26 @@
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+const app = express();
+const path = require("path");
+
+const server = http.createServer(app);
+
+const publicFolder = path.join(__dirname,"./client");
+
+app.use(express.static(publicFolder));
+
+const port = process.env.PORT || 8000
+server.listen(port, () => {
+  console.log("listening on port 8000");
+});
+
+app.get("/", (req, res) => {
+  res.send("index.html");
+});
+
 //Node server which will handle socket io connections
-const io = require('socket.io')(8000)
+const io = socket(server);
 
 const users = {};
 
